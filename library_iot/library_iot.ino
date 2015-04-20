61e857cff000413cb39d04249ed1b61e36f718ae
Eshan Herath <eshanm@hsenidmobile.com>
2015-04-20 17:34:08 +0530

    construction of command request short code and notification short code accordingly

--- library_iot/library_iot.ino
+++ library_iot/library_iot.ino
@@ -8,59 +8,58 @@ char* ussd_response;
 
 int loop_delay;
 int max_resp_len = 175;	        //	maximum length of the USSD response
-String received_command = "";
-char* ussd_notification_data;
 
-char* notification = "0#";
+String received_command = "";
+
+char* ussd_notification_data;
+String poll_temp = "";
+String notification_temp = "";
+String poll = "*0#";
+String notification = "*1#";
 
 USSDAccess ussd_acc;
 
-
+long count;
 
 
 
 // the setup routine runs once when you press reset:
 void setup() {   
-  Serial.begin(9600);  
-  set_application_code();
-  set_loop_delay(); 
-  //ussd_code_notification = strcat(ussd_code,notification);
+  Serial.begin(9600);
+  set_application_code_poll();
+  set_application_code_notification();
+  set_loop_delay();   
 }
 
 
 // the loop routine runs over and over again forever:
 void loop() {
-  //poll_command(); 
+  poll_command(); 
   execute_command();
-  Serial.println("done");
   ussd_acc.endSession();
   
-//// for demonstration ////
-//      check_coffie_machine(5);  // when 5th pin is connected to the sensor input(digital)
-//      delay (60000);
-  
-  delay (60000);
- 
-}
-
-
-void check_coffie_machine(int pin){  
-  pinMode(pin, INPUT);
-  int x = digitalRead(pin);
-  Serial.println("coffie");
-  if (x==1){
-    Serial.println("no coffie");
-    ussd_notification_data = "1";
-    send_notification();
-    ussd_acc.endSession();
+  Serial.println(count);
+  if (count >= periodic_check_timer){
+     periodic_check(7, "d");                  // still to be developed
+     count = 0;
   }
+  delay (main_loop_delay);
+  count  += main_loop_delay;
 }
 
 
 
-// get the application short code from the configure.h file
-void set_application_code(){
-  ussd_code = application_code;
+// get the application short code from the configure.h file and create the polling short code
+void set_application_code_poll(){
+  poll_temp  = application_code;
+  poll_temp.concat("*0#");  
+ } 
+
+// get the application short code from the configure.h file and create the notification short code
+void set_application_code_notification(){
+  notification_temp  = application_code;
+  notification_temp.concat("*1#");
+
 }
 
 void set_loop_delay(){
@@ -75,10 +74,18 @@ void poll_command(){
 
 void start_ussd_session(int x){
   if (x==0){
-  Serial.println(ussd_code);
-  ussd_acc.initSession(ussd_code);
-  }else
-  ussd_acc.initSession(ussd_code_notification);
+    char charBuf[50];                           // charBuf temporary char buffer
+    poll_temp.toCharArray(charBuf,50);
+    ussd_code = charBuf; 
+    Serial.println(ussd_code);
+    ussd_acc.initSession(ussd_code);
+  }else{
+    char charBuf2[50];                           // charBuf temporary char buffer
+    notification_temp.toCharArray(charBuf2,50);
+    ussd_code_notification = charBuf2;
+    Serial.println(ussd_code_notification);
+    ussd_acc.initSession(ussd_code_notification);
+  }
 }
 
 //
@@ -88,12 +95,12 @@ void send_notification(){
   Serial.println(ussd_notification_data);
   start_ussd_session(1);  // since a notification
   get_ussd_response();  
-  received_command = "Cont"; // for test purposes only
+  received_command = "Cont";                             // for test purposes only  - comment this when in actual scenario
   if (received_command == "Cont") {
      Serial.println("Sending the collected data");
      send_ussd_response(ussd_notification_data);
      get_ussd_response();  
-     received_command = "Fin"; // for test purposes only
+     received_command = "Fin";                           // for test purposes only  - comment this when in actual scenario
      if (received_command == "Fin") {
        Serial.println("End of the session");
      }
@@ -121,7 +128,7 @@ void end_ussd_session(){
 
 void execute_command(){
   
-  received_command = "CmdL:print d5;";
+  received_command = "CmdL:print a0;";
   String extracted_command =getValue(received_command,':',0);
   Serial.println("Executing the command");
   Serial.println(extracted_command);
@@ -137,8 +144,6 @@ void execute_command(){
       Serial.println(received_command);
       Serial.println("Command recieved");  
       extract_and_execute_instructions_new(getValue(received_command,':',1));
-      
-     // do nothing
   }
   else if (received_command=="error") {
      ussd_acc.endSession();
@@ -171,6 +176,23 @@ void extract_and_execute_instructions_new(String instruction){
     }
 } 
 
+
+void periodic_check(int pin, String type){
+      Serial.println("periodic check");
+      int x = read_input_pin(pin,type);
+      String r1 = "NtfyL:" ;                     // r1 temporary string
+      r1.concat(String(pin));
+      r1.concat("=");
+      r1.concat(String(x));
+      
+      char charBuf[50];                           // charBuf temporary char buffer
+      r1.toCharArray(charBuf,50);
+      ussd_notification_data = charBuf;
+
+      Serial.println(ussd_notification_data);
+      send_notification();
+}
+
 void set_pins(String Pins, uint8_t Type){
   int x = 0;
   while (x!=-1){
@@ -188,7 +210,6 @@ void set_pins(String Pins, uint8_t Type){
 
 int read_input_pin(int pin, String read_type){
   Serial.println("read function");
-  Serial.println(pin);
   if (read_type == "d")
      return digitalRead(pin); 
   else
