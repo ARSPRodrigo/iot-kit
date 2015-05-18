f06c8d7fe782c8df7fad00a21886b474cf054723
Eshan Mewantha <eshanm@hsenidmobile.com>
2015-05-18 18:51:19 +0530

    command and function execution seperated

--- library_iot/library_iot.ino
+++ library_iot/library_iot.ino
@@ -80,7 +80,7 @@ void setup() {
   addBitlashFunction("run_bitlash_command", (bitlash_function) run_bitlash_command);
   setOutputHandler(&serialHandler);
   register_device();
-  pinMode(7, OUTPUT);
+  //pinMode(7, OUTPUT);
 }
 
 void loop() {
@@ -157,6 +157,10 @@ void send_notification(){
   }
 }
 
+void send_ussd_response(char* ussd_response){
+  ussd_acc.respond(ussd_response);  
+}
+
 void get_ussd_response(){
   char resp[max_resp_len];
   Serial.println("Receiving..");
@@ -167,29 +171,17 @@ void get_ussd_response(){
   Serial.println(received_command);
 }
 
-void send_ussd_response(char* ussd_response){
-  ussd_acc.respond(ussd_response);  
-}
 
-void execute_command(){  
-  received_command = "CmdL:while 1 {if(!d13){print \"light is toggling\";}; snooze(100);};";  
-  //perform the replacements:
-  String temp_string = received_command;
-  temp_string.replace("'", "\"");
-  received_command = temp_string;
-  
-  String extracted_command =getValue(received_command,':',0);   
-  
-  if (received_command=="Fin") {
-    Serial.println("No command received. Nothing to do.");     
+
+void extract_and_execute_instructions_new(String action, String instruction){
+  if (action == "c"){
+    Serial.println("Command is executing.."); 
+    char charBuf[200];                              
+    instruction.toCharArray(charBuf,200);
+    char* bitlash_command = charBuf;
+    doCommand(bitlash_command);
   }
-  else if (extracted_command=="Cmd" || extracted_command=="CmdL") {
-    Serial.println("Command ready: " + getValue(received_command,':',1));  
-    extract_and_execute_instructions_new(getValue(received_command,':',1));     
-  } 
-}
-
-void extract_and_execute_instructions_new(String instruction){
+  else if (action == "f"){ 
   String temp1 = "function iot_function {";        
   temp1.concat(instruction);                      // eg: "function iot_function {pinMode(13, 1); dw(13,1);  if(a0< 200){print \"level=\"a0;}; snooze(2000); }"
   temp1.concat("}");
@@ -205,6 +197,30 @@ void extract_and_execute_instructions_new(String instruction){
   run_bitlash_command();
   Serial.println("Bitlash command running..");  
 } 
+}
+
+
+void execute_command(){  
+  //received_command = "CmdL:while 1 {if(!d13){print \"light is toggling\";}; snooze(100);};";  
+    received_command = "CmdL:f:while 1 {if(!d13){print \"light is toggling\";}; snooze(100);};";  
+  // received_command = "CmdL:c: print d13;;"; 
+  //perform the replacements:
+  String temp_string = received_command;
+  temp_string.replace("'", "\"");
+  received_command = temp_string;
+  
+  String extracted_command =getValue(received_command,':',0);   
+  
+  if (received_command=="Fin") {
+    Serial.println("No command received. Nothing to do.");     
+  }
+  else if (extracted_command=="Cmd" || extracted_command=="CmdL") {
+    Serial.println("Command ready: " + getValue(received_command,':',1));  
+    extract_and_execute_instructions_new(getValue(received_command,':',0),getValue(received_command,':',1));     
+  } 
+}
+
+
 
 
 String getValue(String data, char separator, int index)
